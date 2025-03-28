03fecedd93de8917dd04724629650c3262a9de77
VictorHugoSantos-prog <victor.h.santos34@aluno.senai.br>
2025-03-28 15:48:30 -0300

    enviando atividade02

--- Atividade01/Script.js
+++ Atividade01/Script.js
@@ -1,17 +1,29 @@
 const titulo = document.getElementById("titulo");
+titulo.textContent = "Meus Esportes Favoritos"; // Altera o título
+titulo.style.color = "blue"; // Muda a cor para azul
+
 console.log("getElementById:", titulo);
 
 const esportesPorClasse = document.getElementsByClassName("esporte");
 console.log("getElementsByClassName:", esportesPorClasse);
 
-
 const itensLista = document.getElementsByTagName("li");
 console.log("getElementsByTagName:", itensLista);
-
 
 const primeiroEsporte = document.querySelector(".esporte");
 console.log("querySelector:", primeiroEsporte);
 
-
 const todosEsportes = document.querySelectorAll(".esporte");
 console.log("querySelectorAll:", todosEsportes);
+
+const lista = document.querySelector("ul");
+const novoEsporte1 = document.createElement("li");
+novoEsporte1.textContent = "Tênis";
+novoEsporte1.classList.add("esporte");
+
+const novoEsporte2 = document.createElement("li");
+novoEsporte2.textContent = "Polo Aquático";
+novoEsporte2.classList.add("esporte");
+
+lista.appendChild(novoEsporte1);
+lista.appendChild(novoEsporte2);
